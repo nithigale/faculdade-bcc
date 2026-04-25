ef7244c4562ad069a560a10fa510e0deaebe4361
nithigale <nithigale@gmail.com>
2026-04-24 22:24:26 -0300

    Adicionar prática de concatenação com variáveis em JavaScript
    Criado exemplo utilizando prompt, alert e concatenação para exibir nome e idade do usuário em mensagens personalizadas

---
+++ estudos-javascript/ex-03/aula-02.js
@@ -0,0 +1,8 @@
+let nome = window.prompt('Qual é o seu nome?') // Vai perguntar o nome
+
+window.alert("É um prazer te conhecer, " + nome + "!") //Concatenação 
+
+
+let idade = 18
+
+window.alert("Olá, " + nome + " você tem " + idade + " anos")
