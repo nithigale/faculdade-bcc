ff49f4d0a619c15eb68d621585adf2fe7a154309
nithigale <nithigale@gmail.com>
2026-04-24 22:41:13 -0300

    Aprender conversão de entrada com Number em JavaScript
    Ao testar a soma com prompt, aprendi que os valores retornam como string e que é necessário usar Number() para realizar operações matemáticas corretamente

--- estudos-javascript/ex-03/aula-02.js
+++
@@ -1,8 +0,0 @@
-let nome = window.prompt('Qual é o seu nome?') // Vai perguntar o nome
-
-window.alert("É um prazer te conhecer, " + nome + "!") //Concatenação 
-
-
-let idade = 18
-
-window.alert("Olá, " + nome + " você tem " + idade + " anos")
---
+++ estudos-javascript/ex-03/teste-03.js
@@ -0,0 +1,6 @@
+let n1 = Number(window.prompt("Digite um número: "))
+let n2 = Number(window.prompt("Digite outro número: "))
+
+let total = n1 + n2
+
+window.alert("O número " + n1 + " + o número " + n2 + " resultam em " + total)
