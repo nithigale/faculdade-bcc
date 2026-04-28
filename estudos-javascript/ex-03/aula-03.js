292dfa5d58c502d971437945fdb6b183a2c65ec9
nithigale <nithigale@gmail.com>
2026-04-27 23:15:02 -0300

    Adicionar conprática de conversões, concatenação e template string em JavaScriptversão de número para string
    Implementação de exemplos envolvendo:
    
    - entrada de dados com prompt
    - conversão de string para number (parseInt e parseFloat)
    - operações matemáticas com valores convertidos
    - conversão de number para string (String)
    - concatenação de textos
    - uso de template strings (${ }) para interpolação de valores
    
    Objetivo: consolidar conceitos básicos de manipulação de dados em JavaScript

--- estudos-javascript/ex-03/aula-03.js
+++ estudos-javascript/ex-03/aula-03.js
@@ -6,3 +6,18 @@ let n2 = Number.parseFloat(window.prompt("Digite outro número: "))
 let total = n1 + n2
 
 window.alert("O número " + n1 + " + o número " + n2 + " resulta em " + total)
+
+// Conversão de número para String
+
+let idade = 18
+
+window.alert("Olá sou o Chris e tenho " + String(idade) + " anos")
+
+//Template String
+
+let curso = "CursoemVideo"
+let tema = "TemplateString"
+
+window.alert(`Queria dizer que aprendi sobre ${tema} assistindo os videos do ${curso}`)
+
+window.alert(`A soma entre o número ${n1} e o ${n2} é igual a ${total}`)
