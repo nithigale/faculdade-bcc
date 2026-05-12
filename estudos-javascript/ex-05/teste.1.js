28b1ec536c5b27fa03fca46e95c1014748e673da
nithigale <nithigale@gmail.com>
2026-05-11 22:21:44 -0300

    feat: primeiros passos com DOM e manipulação de elementos
    Estudo introdutório de DOM em JavaScript envolvendo:
    
    - busca de elementos por tag com getElementsByTagName()
    - busca de elementos por id com getElementById()
    - busca utilizando querySelector()
    - manipulação de conteúdo com innerText
    - alteração de estilos CSS com .style
    - interação entre JavaScript e HTML através do DOM
    
    Objetivo: compreender como acessar e modificar elementos da página usando JavaScript

---
+++ estudos-javascript/ex-05/teste.1.js
@@ -0,0 +1,4 @@
+let idade = Number(window.prompt("Qual a sua idade?"))
+let seg = idade >= 18? "Você pode entrar na festa" : "Você não pode entrar na festa"
+
+window.alert(seg)
---
+++ estudos-javascript/ex-05/teste.2.js
@@ -0,0 +1,7 @@
+let logado = false
+if(!logado) {
+    window.alert("Faça login")
+}
+else{
+    window.alert("Bem-vindo")
+}
