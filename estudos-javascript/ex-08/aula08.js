29a7c2ced81cb3bb2b265d2304fd1c5e9410fb15
nithigale <nithigale@gmail.com>
2026-05-15 18:18:06 -0300

    feat: prática de condições e interação com DOM
    Implementação de exercícios envolvendo:
    
    - estruturas condicionais simples e compostas
    - verificação de velocidade com sistema de multas
    - validação de nacionalidade
    - captura de valores de inputs utilizando DOM
    - manipulação de elementos com innerHTML
    - uso de console.log() para exibição no console
    - utilização de template strings para exibir resultados dinâmicos
    
    Objetivo: praticar lógica condicional e interação entre JavaScript, HTML e DOM.

---
+++ estudos-javascript/ex-08/aula08.js
@@ -0,0 +1,6 @@
+let vel = Number(window.write('Você passou no radar a quantos km/h?'))
+if (vel > 50) {
+    console.log('Você ultrapassou o limite de velocidade permitido. Multado!')
+} else {
+    console.log('Você não ultrapassou o limite de velocidade. A multa não será aplicada!')
+}
